05fc83a3c7d4657ef457779c385f3043991c78c1
David <davidtorroija@gmail.com>
2021-06-08 11:38:01 -0500

    - Separated colors into a new scss file - Added AddModal component and added some styles to this component - Developed the Close modal logic - Added some shared classes in the main scss

---
+++ src/components/AddModal.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="AddModal">
+    <div class="AddModal__outer">
+      <div class="AddModal__header">
+        <span>{{ modalTitle }}</span>
+        <button class="btn btn-icon" @click="closeModal">
+          <span class="fa fas fa-times fa-lg"></span>
+        </button>
+      </div>
+      <div class="AddModal__body">Body</div>
+      <div class="AddModal__footer">
+        <button class="btn" @click="closeModal">
+          SAVE
+        </button>
+      </div>
+    </div>
+    <div class="AddModal__backdrop">
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      pepe: 'hello Im a modal',
+    };
+  },
+  methods: {
+    ...mapActions(['getProducts']),
+    closeModal() {
+      this.$emit('close');
+    },
+  },
+  props: {
+    modalTitle: {
+      type: String,
+      default: 'Add Product',
+    },
+  },
+  // computed: {
+  //   ...mapGetters(['products']),
+  // },
+  // created() {
+  //   this.getProducts();
+  // },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+  .AddModal {
+    &__backdrop {
+      opacity: 0.5;
+      background-color: var(--white);
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 1050;
+      width: 100%;
+      height: 100%;
+    }
+    &__body {
+      flex-grow: 1;
+    }
+    &__header {
+      justify-content: space-between;
+      display: flex;
+      align-items: center;
+    }
+    &__outer {
+      position: absolute;
+      margin: 0 auto;
+      background-color: var(--gray);
+      height: 80%;
+      width: 50%;
+      z-index: 1060;
+      // border: 1rem solid var(--white);
+      border-radius: 0.5rem;
+      padding: 0.5rem;
+      display: flex;
+      flex-direction: column;
+    }
+  }
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <button>ADD <span class="btn fa fas fa-plus"></span></button>
+    <AddModal @close="closeAddModal" v-if="isAddModalVisible"/>
+    <h1> Code test for David Torroija</h1>
+    <button class="btn margin-y" @click="showAddProductModal">
+      ADD
+      <span class="fa fas fa-plus"></span>
+    </button>
     <div class="row px-1">
       <template v-for="(product, index) in products">
           <div
@@ -9,12 +13,12 @@
               class="ProductCard"
           >
             <img
-              src="https://images.unsplash.com/photo-1622495551393-9b3c4801ae14?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
+              :src="product.imageURL"
               alt="some Image"
               class="ProductCard__image"
             >
             <span class="ProductCard__description">
-              {{ product.name }}
+              {{ product.description }}
             </span>
           </div>
       </template>
@@ -42,14 +46,27 @@
 <script>
 
 import { mapActions, mapGetters } from 'vuex';
+import AddModal from './AddModal.vue';
 
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String,
+  data() {
+    return {
+      isAddModalVisible: false,
+    };
+  },
+  components: {
+    AddModal,
   },
   methods: {
     ...mapActions(['getProducts']),
+    showAddProductModal() {
+      this.isAddModalVisible = true;
+      console.log('aca');
+    },
+    closeAddModal() {
+      this.isAddModalVisible = false;
+    },
   },
   computed: {
     ...mapGetters(['products']),
