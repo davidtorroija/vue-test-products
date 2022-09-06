bec05854b01bf2429934e7b3bf4fdb229351f06e
David Torroija <dtorroija@altimetrik.com>
2022-09-06 11:57:45 -0500

    adding style to the nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <router-view/>
+    <router-link to="/">Dashboard</router-link> |
+    <router-link to="/about">About</router-link>
+  <router-view/>
   </div>
 </template>
