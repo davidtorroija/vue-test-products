975b7949711a55ac9079f85d06a81c2c8094b177
David Torroija <dtorroija@altimetrik.com>
2022-09-07 13:17:04 -0500

    added hide completed

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -6,17 +6,25 @@
       <button>Add Todo</button>
     </form>
     <ul>
-      <li v-for="todo in todos" :key="todo.id">
+      <li v-for="todo in filteredTodos" :key="todo.id" :class="todo.done ? 'done' : ''">
+        <input type="checkbox" v-model="todo.done"/>
         {{ todo.text }}
         <button @click="removeTodo(todo)">X</button>
       </li>
     </ul>
+    <button @click="hideCompleted = !hideCompleted">
+      {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+    </button>
   </div>
+
 </template>
 <style>
 body {
   background: black;
   color: white;
+}
+.done {
+  text-decoration: line-through;
 }
 </style>
 <script>
@@ -24,13 +32,14 @@ let id = 4;
 export default {
   data() {
     const todos = [];
-    todos.push({ id: 1, text: 'Learn HTML' });
-    todos.push({ id: 2, text: 'Learn HTML' });
-    todos.push({ id: 3, text: 'Learn HTML' });
+    todos.push({ id: 1, text: 'Learn HTML', done: true });
+    todos.push({ id: 2, text: 'Learn 2' });
+    todos.push({ id: 3, text: 'Learn 3', done: false });
 
     return {
       newTodo: '',
       todos,
+      hideCompleted: false,
     };
   },
   methods: {
@@ -41,6 +50,14 @@ export default {
     },
     removeTodo(todo) {
       this.todos = this.todos.filter((item) => item.id !== todo.id);
+    },
+  },
+  computed: {
+    filteredTodos() {
+      if (this.hideCompleted) {
+        return this.todos.filter((todo) => !todo.done);
+      }
+      return this.todos;
     },
   },
 };
